2d2f32dd6c6085a15d66b31e4874b2a3ec42bc55
Jader Vanderlinde <jadercesarvanderlinde12@gmail.com>
2025-11-02 12:37:08 -0300

    Inicializando projeto

---
+++ src/test/java/com/example/appcore/appcore/AppcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcore.appcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
