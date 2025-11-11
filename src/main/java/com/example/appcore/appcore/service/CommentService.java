d61364122b4a4dfd887bad70cf57210bf64315bf
Jader Vanderlinde <jadercesarvanderlinde12@gmail.com>
2025-11-11 19:57:05 -0300

    Criando service

--- src/main/java/com/example/appcore/appcore/service/CommentService.java
+++ src/main/java/com/example/appcore/appcore/service/CommentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -15,5 +16,26 @@ public class CommentService {
 
     public List<Comment> getComments() {
         return commentRepository.findAll();
+    }
+
+    public Optional<Comment> getComment(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public Comment update(Long id, Comment comment) {
+        Comment existing = commentRepository.findById(id).orElseThrow(() -> new RuntimeException("Comentário com o id" + id + " não encontrado"));
+
+        existing.setContent(comment.getContent());
+        existing.setAuthorName(comment.getAuthorName());
+        existing.setRating(comment.getRating());
+
+        return commentRepository.save(existing);
+    }
+    public void deleteComment(Long id) {
+         commentRepository.deleteById(id);
     }
 }
